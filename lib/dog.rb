86f9d1b4f30a573a32d998caf7fd5f23078903d2
Grace5275 <grace_madson@outlook.com>
2018-07-30 13:48:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # dog.rb
+
+class Dog
+  def initialize= (fido)
+    fido = fido
+  end 
+  def snoopy= (snoopy)
+    @snoopy = snoopy
+  end
+  def lassie= (lassie)
+    @lassie = lassie
+  end 
+end
+  
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,10 @@
 # person.rb
+
+class Person
+  def adele_goldberg= (adele_goldberg)
+    @adele_goldberg = adele_goldberg
+  end 
+  def alan_key= (alan_key)
+    @alan_key = alan_key
+  end 
+end 
